ec29a02a66b7e14cbed7403209d9a89b36550da5
SetMod <voroncka.andrey@gmail.com>
2021-09-22 13:39:53 +0300

    IA-115 fead: added new shell script

--- Automation/build_geocitizen.sh
+++ Automation/build_geocitizen.sh
@@ -3,15 +3,13 @@
 GEO_PATH=$1
 # saving current dirrectory
 CUR_DIR=`pwd`
-DO_BUILD='no'
-DO_INSTAll='no'
-DO_COPY_FRONT='no'
-DO_COPY_WAR='no'
+
 
 if [[ "$GEO_PATH" == "" ]];
 then
 GEO_PATH=`pwd`
 fi
+
 
 if  [[ ! -d "$GEO_PATH" ]]; # check if PATH valid
 then
@@ -20,66 +18,43 @@ then
     exit 1
 fi
 
-echo -e "Do build front-end?[yes|no] default:no:"
-read DO_BUILD
 
-if [[ "$DO_BUILD" == "yes" ]];
-then
-    # building fron end
-    printf "\nBuilding Geocitizen FRONT END in $GEO_PATH""front-end/"
-    cd "$GEO_PATH""front-end/"
-    sudo npm run build
-    echo -e "Done\n"
-fi
+# building fron end
+printf "\nBuilding Geocitizen FRONT END in $GEO_PATH""front-end/"
+cd "$GEO_PATH""front-end/"
+sudo npm run build
+echo -e "Done\n"
 
-echo -e "Do copy front-end?[yes|no] default:no:"
-read DO_COPY_FRONT
 
-if [[ "$DO_COPY_FRONT" == "yes" ]];
-then
-    # copy all from /front-end/dist to /src/main/webapp/
-    printf "Copy all from /front-end/dist to /src/main/webapp/"
-    sudo rsync -a "$GEO_PATH""front-end/dist/" "$GEO_PATH""src/main/webapp/"
-    echo -e "Done\n"
-fi
+# copy all from /front-end/dist to /src/main/webapp/
+printf "Copy all from /front-end/dist to /src/main/webapp/"
+sudo rsync -a "$GEO_PATH""front-end/dist/" "$GEO_PATH""src/main/webapp/"
+echo -e "Done\n"
 
-echo -e "Edit index.html?[yes|no] default:no:"
-read DO_COPY_FRONT
-if [[ "$DO_COPY_FRONT" == "yes" ]];
-then
-    echo "Editing index.html in /src/main/webapp/"
-    # edit index.html in /src/main/webapp/ (/src/assets -> ./static/)
-    sed -i "s/[.]*\?\/src\/assets/.\/static/" "$GEO_PATH""src/main/webapp/index.html"
-    # edit index.html in /src/main/webapp/ (/static/css/ -> ./static/css/)
-    sed -i "s/[.]*\?\/static\/css/.\/static\/css/" "$GEO_PATH""src/main/webapp/index.html"
-    # edit index.html in /src/main/webapp/ (/static/js/ -> ./static/js/)
-    sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "$GEO_PATH""src/main/webapp/index.html"
-    sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "$GEO_PATH""src/main/webapp/index.html"
-    echo -e "Done\n"
-fi
 
-echo -e "Do mvn install?[yes|no] default:no:"
-read DO_INSTAll
+echo "Editing index.html in /src/main/webapp/"
+# edit index.html in /src/main/webapp/ (/src/assets -> ./static/)
+sed -i "s/[.]*\?\/src\/assets/.\/static/" "$GEO_PATH""src/main/webapp/index.html"
+# edit index.html in /src/main/webapp/ (/static/css/ -> ./static/css/)
+sed -i "s/[.]*\?\/static\/css/.\/static\/css/" "$GEO_PATH""src/main/webapp/index.html"
+# edit index.html in /src/main/webapp/ (/static/js/ -> ./static/js/)
+sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "$GEO_PATH""src/main/webapp/index.html"
+sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "$GEO_PATH""src/main/webapp/index.html"
+echo -e "Done\n"
 
-if [[ "$DO_INSTAll" == "yes" ]];
-then
-    # doing maven install
-    echo "Doing mvn install in $GEO_PATH"
-    cd "$GEO_PATH"
-    sudo mvn install
-    echo -e "Done\n"
-fi
 
-echo -e "Do copy citizen.war?[yes|no] default:no:"
-read DO_COPY_WAR
+# doing maven install
+echo "Doing mvn install in $GEO_PATH"
+cd "$GEO_PATH"
+sudo mvn install
+echo -e "Done\n"
 
-if [[ "$DO_COPY_WAR" == "yes" ]];
-then
-    # copy citizen.war from /target to /opt/latest/webapps/
-    printf "Copy citizen.war from /target/ to /opt/tomcat/latest/webapps/"
-    sudo cp "$GEO_PATH""target/citizen.war" "/opt/tomcat/latest/webapps/"
-    echo -e "\nDone\n"
-fi
+
+# copy citizen.war from /target to /opt/latest/webapps/
+printf "Copy citizen.war from /target/ to /opt/tomcat/latest/webapps/"
+sudo cp "$GEO_PATH""target/citizen.war" "/opt/tomcat/latest/webapps/"
+echo -e "\nDone\n"
+
 
 cd "$CUR_DIR"
 exit 1
