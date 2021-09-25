665a02612a26aa82fc4fd345c6c5f7e642f32adc
SetMod <voroncka.andrey@gmail.com>
2021-09-25 20:59:23 +0300

    IA-115 fix: reverted fixs

--- Automation/build_copy_edit.sh
+++ Automation/build_copy_edit.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+cd "./front-end/"
+npm install
+npm audit fix
+npm run build
+cd ..
 cp -r "./front-end/dist/." "./src/main/webapp/"
 sed -i "s/[.]*\?\/src\/assets/.\/static/" "./src/main/webapp/index.html"
 sed -i "s/[.]*\?\/static\/css/.\/static\/css/" "./src/main/webapp/index.html"
 sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "./src/main/webapp/index.html"
 sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "./src/main/webapp/index.html"
 sed -i "s/[.]*\?\/static\/js/.\/static\/js/g" "./src/main/webapp/index.html"
+mvn install
+
+exit 1
--- Automation/replace_ip_and_port_geocitizen.sh
+++ Automation/replace_ip_and_port_geocitizen.sh
@@ -2,6 +2,14 @@
 
 DB_IP=$1
 WEB_IP=$2
+REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
+
+if  [[ ! "$DB_IP" =~ $REGEXP || ! "$WEB_IP" =~ $REGEXP ]]; # check if IP and PATH valid
+then
+    printf "Set the Servers IPs(0.0.0.0):"
+    printf "\n\n\tExample: $0 DB_IP WEB_IP\n"
+    exit 1
+fi
 
 sed -i "s/localhost/$WEB_IP/" "./front-end/src/main.js"
 sed -i "s/localhost/$WEB_IP/" "./front-end/config/index.js"    
@@ -10,3 +18,5 @@ sed -i "s/localhost:8080/$WEB_IP:8080/" "./src/main/resources/application.proper
 sed -i "s/db.url=jdbc:postgresql:\/\/localhost/db.url=jdbc:postgresql:\/\/$DB_IP/" "./src/main/resources/application.properties"
 sed -i "s/\^1.0.0-beta-7/1.0.0-beta-8/" "./front-end/package.json"
 sed -i "s/\"vue-router\": \"^3.0.1\"/\"vue-router\": \"3.0.1\"/" "./front-end/package.json"
+
+exit 1
