4dc4a61cafce2f75b3893a2a99d15cea0bf48ebf
SetMod <voroncka.andrey@gmail.com>
2021-09-25 20:52:45 +0300

    IA-115 fix: fixed all files

--- Automation/replace_ip_and_port_geocitizen.sh
+++ Automation/replace_ip_and_port_geocitizen.sh
@@ -2,14 +2,6 @@
 
 DB_IP=$1
 WEB_IP=$2
-REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
-
-if  [[ ! "$DB_IP" =~ $REGEXP || ! "$WEB_IP" =~ $REGEXP ]]; # check if IP and PATH valid
-then
-    printf "Set the Servers IPs(0.0.0.0):"
-    printf "\n\n\tExample: $0 DB_IP WEB_IP\n"
-    exit 1
-fi
 
 sed -i "s/localhost/$WEB_IP/" "./front-end/src/main.js"
 sed -i "s/localhost/$WEB_IP/" "./front-end/config/index.js"    
