e34335bbaef2ae432902c66a63c2538f0e502f89
Denis Savchuk <kbob88@gmail.com>
2021-10-05 20:29:35 +0300

    Update script dots.sh

--- scripts/dots.sh
+++ scripts/dots.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-sed -i 's/<link href=/&./' ./src/main/webapp/index.html
-sed -i 's/<script type=text\/javascript src=/&./g' ./src/main/webapp/index.html
+sed -i "s!<link href=/!<link href=./!g" ./src/main/webapp/index.html
+sed -i "s!<script type=text/javascript src=/!<script type=text/javascript src=./!g" ./src/main/webapp/index.html
